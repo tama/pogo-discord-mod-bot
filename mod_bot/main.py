dd3ab1b0b4749aeb20408800849547325ed21a41
tama <tama.boss@gmail.com>
2019-05-02 15:13:43 +0200

    Whitespace & diff

--- mod_bot/main.py
+++ mod_bot/main.py
@@ -322,6 +322,7 @@ async def modtask():
                 # Recherche dans les 5 derniers messages le dernier ne
                 # provenant pas de modbot
                 last_message = None
+                print(channel.name.encode())
                 async for message in channel.history(limit = 5):
                     if last_message is None:
                         last_message = message
@@ -343,7 +344,7 @@ async def modtask():
                     time_since_modbot = now_tz - last_modbot_tz
                 else:
                     time_since_modbot = datetime.timedelta(seconds=0)
-
+    
                 time_delta = now_tz - last_message_tz
                 time_since_end = now_tz - end_time_tz
 
