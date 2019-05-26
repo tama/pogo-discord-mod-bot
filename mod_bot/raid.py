f7aa5ced6cd98bebbb8d6d78b5492d3e64207fa0
Whitefret <nicolas_blaye@hotmail.fr>
2019-05-26 23:48:54 +0200

    Add time remaining in raid command

--- mod_bot/raid.py
+++ mod_bot/raid.py
@@ -1,3 +1,53 @@
+from datetime import datetime
+from datetime import timedelta
+
+raid_date_format = ['%Hh%M', '@%Hh%M', '%M', '%Mmn', '%Mmin', '%Mminutes']
+
+
+def get_raid_hours(time, raid_duration, message_date):
+    """
+    Raid hour can be declare in 3 different ways:
+    - @hh:mm|@h:mm raid starts at the given hour
+    - hh:mm|h:mm raid ends at the given hour
+    - mm|m raid ends in m|mm minutes
+    :param time: hour or duration
+    :param raid_duration: duration of a raid in minutes
+    :param message_date: time the raid command was issued
+    :return: starttime: time, endtime: time
+    """
+
+    is_at_time = '@' in time
+    is_minutes_time = 'h' not in time
+
+    raid_time = try_parsing_date(time)
+
+    if raid_time is None:
+        return None
+
+    if is_at_time:
+        starttime = raid_time
+        endtime = raid_time + timedelta(minutes=raid_duration)
+    else:
+        if is_minutes_time:
+            minutes = raid_time.minute
+            starttime = message_date
+            endtime = message_date + timedelta(minutes=minutes)
+        else:  # Todo: might be not useful with the minutes remaining command
+            starttime = raid_time - timedelta(minutes=raid_duration)
+            endtime = raid_time
+
+    return starttime.time(), endtime.time()
+
+
+def try_parsing_date(text):
+    for fmt in raid_date_format:
+        try:
+            return datetime.strptime(text, fmt)
+        except ValueError:
+            pass
+    return None
+
+
 def clean_raid_command(command):
     """
     - Remove empty parts
--- test/test_raid_creation.py
+++ test/test_raid_creation.py
@@ -1,22 +1,59 @@
 import unittest
 
-from mod_bot.raid import clean_raid_command, clean_empty_parts, fix_lone_at_sign, fix_lone_exclamation_sign
+from mod_bot import raid
+from datetime import time
+from datetime import datetime
+import pytz
 
 
 class TestRaidCreation(unittest.TestCase):
+    global now
+    now = datetime.now(pytz.timezone('Europe/Paris'))
 
     def test_clean_spaces(self):
         words = ["!raid", " ", "egg5", "arena", "@20h00"]
-        self.assertEqual(clean_empty_parts(words), ["!raid", "egg5", "arena", "@20h00"])
+        self.assertEqual(raid.clean_empty_parts(words), ["!raid", "egg5", "arena", "@20h00"])
 
     def test_fix_lone_at_sign(self):
         words = ["!raid", " ", "egg5", "arena", "@", "20h00"]
-        self.assertEqual(fix_lone_at_sign(words), ["!raid", " ", "egg5", "arena", "@20h00"])
+        self.assertEqual(raid.fix_lone_at_sign(words), ["!raid", " ", "egg5", "arena", "@20h00"])
 
     def test_fix_lone_exclamation_sign(self):
         words = ["!", "raid", " ", "egg5", "arena", "@20h00"]
-        self.assertEqual(fix_lone_exclamation_sign(words), ["!raid", " ", "egg5", "arena", "@20h00"])
+        self.assertEqual(raid.fix_lone_exclamation_sign(words), ["!raid", " ", "egg5", "arena", "@20h00"])
 
     def test_clean_raid_command(self):
         words = ["!", "raid", "", "egg5", "arena", "@", "20h00"]
-        self.assertEqual(clean_raid_command(words), ["!raid", "egg5", "arena", "@20h00"])
+        self.assertEqual(raid.clean_raid_command(words), ["!raid", "egg5", "arena", "@20h00"])
+
+    def test_raid_at_time(self):
+        hour = "@20h00"
+        self.assertEqual(raid.get_raid_hours(hour, 45, now), (time(20, 0), time(20, 45)))
+        hour = "@9h12"
+        self.assertEqual(raid.get_raid_hours(hour, 10, now), (time(9, 12), time(9, 22)))
+
+    def test_raid_end_time(self):
+        hour = "20h45"
+        self.assertEqual(raid.get_raid_hours(hour, 45, now), (time(20, 0), time(20, 45)))
+        hour = "9h22"
+        self.assertEqual(raid.get_raid_hours(hour, 10, now), (time(9, 12), time(9, 22)))
+
+    def test_format_date_minutes(self):
+        minutes = "43"
+        self.assertEqual(raid.try_parsing_date(minutes).time(), time(0, 43))
+        minutes = "43mn"
+        self.assertEqual(raid.try_parsing_date(minutes).time(), time(0, 43))
+        minutes = "43min"
+        self.assertEqual(raid.try_parsing_date(minutes).time(), time(0, 43))
+        minutes = "43minutes"
+        self.assertEqual(raid.try_parsing_date(minutes).time(), time(0, 43))
+        minutes = "-10"
+        self.assertEqual(raid.try_parsing_date(minutes), None)
+        minutes = "70"  # TODO: Should work for events
+        self.assertEqual(raid.try_parsing_date(minutes), None)
+
+    def test_format_date_hour(self):
+        hours = "10h11"
+        self.assertEqual(raid.try_parsing_date(hours).time(), time(10, 11))
+        hours = "@10h11"
+        self.assertEqual(raid.try_parsing_date(hours).time(), time(10, 11))
